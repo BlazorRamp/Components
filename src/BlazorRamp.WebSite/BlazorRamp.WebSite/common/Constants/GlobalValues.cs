01a93058412785e3fbfa4350d9a3de30134569ec
Paul Kent <paulandrewkent@hotmail.com>
2026-01-07 15:26:51 +0000

    Web site project for accessibility checks of the components

---
+++ src/BlazorRamp.WebSite/BlazorRamp.WebSite/common/Constants/GlobalValues.cs
@@ -0,0 +1,52 @@
+﻿namespace BlazorRamp.WebSite.common.Constants;
+public static class GlobalValues
+{
+
+    public const string JS_Module_File_Path   = "./assets/js/test-component.js";
+    public const string JS_Initialise_Func    = "initialise";
+    public const string JS_Check_Close_Nav    = "checkCloseSideNavigation";
+    public const string JS_Show_Modal_Dialog  = "showModalDialog";
+    public const string JS_Close_Modal_Dialog = "closeModalDialog";
+
+    public const string Path_About_Blazor_Ramp = "/about-blazor-ramp";
+    public const string Path_Home                 = "/";
+    public const string Path_Busy_Screen          = "/busy-page";
+    public const string Path_Busy_Container       = "/busy-container";
+    public const string Path_Modal_Busy_Screen    = "/modal-busy-page";
+    public const string Path_Busy_Short           = "/busy-short";
+    public const string Path_Final_Words          = "/final-words";
+
+    public const string Title_About_Blazor_Ramp    = "About Blazor Ramp";
+    public const string Title_Busy_Screen          = "Busy Page Test";
+    public const string Title_Home_Screen          = "Home";
+    public const string Title_Busy_Container       = "Busy Container Test";
+    public const string Title_Modal_Busy_Screen    = "Modal Busy Page Test";
+    public const string Title_Busy_Short           = "Busy With Short Delay Test";
+    public const string Title_Final_Words          = "Final Words";
+
+    public const string Main_Navigation_ID = "main-navigation";
+
+    public const string Main_Navigation_Class = "main-navigation";
+    public const string Main_Navigation_Link_Class = $"{Main_Navigation_Class}__link";
+
+    public const string Theme_Dark_Text = "The site is now using a dark coloured theme.";
+    public const string Theme_Light_Text = "The site is now using a light coloured theme.";
+
+
+    public const string Info_Box_Class                      = "info-box";
+    public const string Info_Box_Heading_Class              = $"{Info_Box_Class}__heading";
+    public const string Info_Box_Heading_Primary_Modifier   = $"{Info_Box_Class}__heading--primary";
+    public const string Info_Box_Coloured_Modifier          = $"{Info_Box_Class}--coloured";
+    public const string Info_Box_Container_Test_Modifier    = $"{Info_Box_Class}--container-test";
+
+    public const string Button_Test_Class = $"test-button";
+    public const string Link_Button_Class = $"link-button";
+    public const string Link_Class        = $"link";
+
+    public const string Link_Button_Right_Arrow_Modifier = $"{Link_Button_Class}--right-arrow";
+    public const string Link_Button_Left_Arrow_Modifier = $"{Link_Button_Class}--left-arrow";
+
+    public const string Modal_Screen_Dialog_Title_ID = "Screen-Dialog-Title-ID";
+
+    public const int Start_Width_For_Collapsed_Menu = 576;
+}
