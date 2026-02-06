b7e08d61b67266802093f188a7e6aff2c996f3fc
Paul Kent <paulandrewkent@hotmail.com>
2026-02-06 17:13:23 +0000

    Started work on altering the test site

--- src/BlazorRamp.WebSite/BlazorRamp.WebSite/common/Constants/GlobalValues.cs
+++ src/BlazorRamp.WebSite/BlazorRamp.WebSite/common/Constants/GlobalValues.cs
@@ -8,21 +8,40 @@ public static class GlobalValues
     public const string JS_Show_Modal_Dialog  = "showModalDialog";
     public const string JS_Close_Modal_Dialog = "closeModalDialog";
 
-    public const string Path_About_Blazor_Ramp = "/about-blazor-ramp";
-    public const string Path_Home                 = "/";
-    public const string Path_Busy_Screen          = "/busy-page";
-    public const string Path_Busy_Container       = "/busy-container";
-    public const string Path_Modal_Busy_Screen    = "/modal-busy-page";
-    public const string Path_Busy_Short           = "/busy-short";
-    public const string Path_Final_Words          = "/final-words";
 
-    public const string Title_About_Blazor_Ramp    = "About Blazor Ramp";
-    public const string Title_Busy_Screen          = "Busy Page Test";
-    public const string Title_Home_Screen          = "Home";
-    public const string Title_Busy_Container       = "Busy Container Test";
-    public const string Title_Modal_Busy_Screen    = "Modal Busy Page Test";
-    public const string Title_Busy_Short           = "Busy With Short Delay Test";
-    public const string Title_Final_Words          = "Final Words";
+
+
+    public const string Page_Title_Overview_Introduction = "Introduction";
+    public const string Page_Title_Overview_About        = "About Blazor Ramp";
+
+    public const string Page_Title_Busy_Screen           = "Overview & Page Test";
+    public const string Page_Title_Busy_Container        = "Busy Container Test";
+    public const string Page_Title_Modal_Busy_Screen     = "Modal Busy Page Test";
+    public const string Page_Title_Busy_Short            = "Busy With Short Delay Test";
+    public const string Page_Title_Final_Words = "Final Words";
+
+    public const string Page_Title_SkipTo_Overview_Test = "Overview & Tests";
+
+    public const string Path_Overview_Introduction = "/";
+    public const string Path_Overview_About = "/about-blazor-ramp";
+
+    public const string Path_SkipTo_Overview = "/skip-to/overview-tests";
+
+    public const string Path_Busy_Screen        = "/busy-indicator/busy-page";
+    public const string Path_Busy_Container     = "/busy-indicator/busy-container";
+    public const string Path_Busy_Modal_Screen  = "/busy-indicator/modal-busy-page";
+    public const string Path_Busy_Short         = "/busy-indicator/busy-short";
+    public const string Path_Faqs_Final_Words   = "/final-words";
+
+
+    public const string Main_Nav_Section_Heading_Overview      = "Overview";
+    public const string Main_Nav_Section_Heading_BusyIndicator = "Busy Indicator";
+    public const string Main_Nav_Section_Heading_SkipTo        = "Skip To";
+    public const string Main_Nav_Section_Heading_Switch        = "Switch";
+    public const string Main_Nav_Section_Heading_ModalDialog   = "Modal Dialog Framework";
+    public const string Main_Nav_Section_Heading_FAQs = "FAQs";
+
+
 
     public const string Main_Navigation_Aria_Label = "Main";
 
@@ -30,6 +49,9 @@ public static class GlobalValues
 
     public const string Main_Navigation_Class = "main-navigation";
     public const string Main_Navigation_Link_Class = $"{Main_Navigation_Class}__link";
+    public const string Main_Navigation_Heading_Class = $"{Main_Navigation_Class}__heading";
+
+
 
     public const string Theme_Dark_Text = "The site is now using a dark coloured theme.";
     public const string Theme_Light_Text = "The site is now using a light coloured theme.";
