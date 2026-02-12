18af7efd20ece74ee079b6d63964e171f7a2eb7b
Paul Kent <paulandrewkent@hotmail.com>
2026-02-12 17:14:18 +0000

    Sandbox code for chekcing varisous placements with nested dialogs

---
+++ src/BlazorRamp.DialogFramework/Sandbox/Sandbox.Client/Common/Models/All.cs
@@ -0,0 +1,5 @@
+﻿namespace Sandbox.Client.Common.Models;
+
+public record class SomePersonData(string FirstName, string Surname, int Age, string Country);
+
+public record SomePersonView(string FullName, string Spouse);
