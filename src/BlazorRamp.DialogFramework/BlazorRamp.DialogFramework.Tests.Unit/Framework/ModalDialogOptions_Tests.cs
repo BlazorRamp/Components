68971808b376c373da16fdb5e5a8c101258ae823
Paul Kent <paulandrewkent@hotmail.com>
2026-02-18 11:44:27 +0000

    Moving tests over from previous project where I had the dialog and checking along the way

---
+++ src/BlazorRamp.DialogFramework/BlazorRamp.DialogFramework.Tests.Unit/Framework/ModalDialogOptions_Tests.cs
@@ -0,0 +1,49 @@
+﻿using BlazorRamp.DialogFramework.Common.Constants;
+using BlazorRamp.DialogFramework.Framework;
+using Bunit;
+using FluentAssertions;
+
+namespace BlazorRamp.DialogFramework.Tests.Unit.Framework
+{
+    public class ModalDialogOptions_Tests
+    {
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void The_constructor_should_set_all_properties_or_use_the_defaults_if_not_provided(bool hasValues)
+        {
+            if (hasValues)
+            {
+
+                var modalDialogOptions = new ModalDialogOptions(HorizontalAlignment.Left, VerticalAlignment.Bottom, 50);
+
+                modalDialogOptions.Should().Match<ModalDialogOptions>(o => o.HorizonalPosition == "start" && o.VerticalPosition == "end" && o.MaxWidth == "50%");
+
+                return;
+            }
+
+            _ = new ModalDialogOptions().Should().Match<ModalDialogOptions>(o => o.HorizonalPosition == "center" && o.VerticalPosition == "center" && o.MaxWidth == "70%");
+
+        }
+
+        [Fact]
+        public void A_width_over_100_should_be_capped_at_to_100_percent()
+        {
+            var modalDialogOptions = new ModalDialogOptions(HorizontalAlignment.Right, VerticalAlignment.Top, 101);
+
+            modalDialogOptions.Should().Match<ModalDialogOptions>(o => o.HorizonalPosition == "end" && o.VerticalPosition == "start" && o.MaxWidth == "100%");
+
+        }
+
+        [Fact]
+        public void Invalid_horizontal_or_vertical_values_should_default_to_center()
+        {
+            var modalDialogOptions = new ModalDialogOptions((HorizontalAlignment) 99, (VerticalAlignment) 99, 50);
+
+            modalDialogOptions.Should().Match<ModalDialogOptions>(o => o.HorizonalPosition == "center" && o.VerticalPosition == "center" && o.MaxWidth == "50%");
+        }
+
+        //TODO Not sure if there is any point altering the options class to check for width below 0
+    }
+    
+}
---
+++ src/BlazorRamp.DialogFramework/BlazorRamp.DialogFramework.Tests.Unit/Framework/ModalDialogWindow_Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorRamp.DialogFramework.Tests.Unit.Framework
+{
+    internal class ModalDialogWindow_Tests
+    {
+    }
+}
