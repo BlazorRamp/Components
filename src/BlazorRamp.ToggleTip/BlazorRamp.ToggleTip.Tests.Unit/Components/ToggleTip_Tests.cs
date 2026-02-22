feb7ab4b64717582c67b7b57d70d86ca3c4c2cb4
Paul Kent <paulandrewkent@hotmail.com>
2026-02-22 11:24:06 +0000

    Solution setup for the ToggleTip component

---
+++ src/BlazorRamp.ToggleTip/BlazorRamp.ToggleTip.Tests.Unit/Components/ToggleTip_Tests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorRamp.ToggleTip.Tests.Unit.Components;
+
+internal class ToggleTip_Tests
+{
+}
