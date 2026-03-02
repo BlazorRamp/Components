2f6b7bab18cae17ebf12be36528db556c6332960
Paul Kent <paulandrewkent@hotmail.com>
2026-03-02 08:52:37 +0000

    Fleshed out the tabs solution to start work. Changed toggleip pack to v1.0.0 as I decided not to do a patch only for the readme which has been corrected.

---
+++ src/BlazorRamp.Tabs/BlazorRamp.Tabs/Common/Constants/GlobalValues.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorRamp.Tabs.Common.Constants;
+
+internal class GlobalValues
+{
+
+}
