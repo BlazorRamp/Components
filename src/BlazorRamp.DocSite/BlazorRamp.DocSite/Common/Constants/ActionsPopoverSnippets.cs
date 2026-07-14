feb5dcf21942969a7d30b492404ebe40488878cc
Paul Kent <paulandrewkent@hotmail.com>
2026-07-14 10:42:19 +0100

    in progress added to do an interim deployment

---
+++ src/BlazorRamp.DocSite/BlazorRamp.DocSite/Common/Constants/ActionsPopoverSnippets.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorRamp.DocSite.Common.Constants;
+
+public class ActionsPopoverSnippets
+{
+    public const string Add_Actions_Popover_Style_Sheet = """
+        <head>
+            <link rel="stylesheet" href="_content/BlazorRamp.Core/assets/css/core.min.css" />
+            <link rel="stylesheet" href="_content/BlazorRamp.ActionsPopover/assets/css/actions-popover.min.css" />
+        </head>
+        """;
+}
