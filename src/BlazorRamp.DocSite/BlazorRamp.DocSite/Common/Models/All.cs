4de0049d921abd09e9169361cbdf5cf1fcc14783
Paul Kent <paulandrewkent@hotmail.com>
2026-08-03 16:52:35 +0100

    minor changes to data table css

--- src/BlazorRamp.DocSite/BlazorRamp.DocSite/Common/Models/All.cs
+++ src/BlazorRamp.DocSite/BlazorRamp.DocSite/Common/Models/All.cs
@@ -3,3 +3,5 @@
 public record class SomePersonData(string FirstName, string Surname, int Age, string Country);
 
 public record SomePersonView(string FullName, string Spouse);
+
+public record class Contact(int ContactID, string Title, string GivenName, string FamilyName, DateOnly DateOfBirth, string Country, decimal Rate);
