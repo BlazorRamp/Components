e9e788c537f911a63d62dffb3a5ed43289cf2c57
Paul Kent <paulandrewkent@hotmail.com>
2026-09-10 14:08:09 +0100

    amended all tables to use the css classes table class.

---
+++ src/BlazorRamp.DocSite/BlazorRamp.DocSite/Common/Extensions/CssClassExtensions.cs
@@ -0,0 +1,11 @@
+﻿using BlazorRamp.CssClasses.Common.Utilities;
+namespace BlazorRamp.DocSite.Common.Extensions;
+
+public static class TableExtensions
+{
+    // Notice the extension block has NO receiver parameter name (extension(Table))
+    extension(Table)
+    {
+        public static string CodeTable => "br-table--code-table";
+    }
+}
